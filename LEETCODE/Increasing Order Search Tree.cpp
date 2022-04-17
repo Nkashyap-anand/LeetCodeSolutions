4347dc8d39b0e61ce98c0538357e088179348d85
Nkashyap-anand <55659236+Nkashyap-anand@users.noreply.github.com>
2022-04-17 22:47:26 +0530

    897. Increasing Order Search Tree

--- LEETCODE/Increasing Order Search Tree.cpp
+++ LEETCODE/Increasing Order Search Tree.cpp
@@ -16,3 +16,35 @@ public:
         return ans;
     }
 };
+
+// easy to understand recursive approach
+
+class Solution
+{
+public:
+    TreeNode *newroot, *curr;
+
+    TreeNode *increasingBST(TreeNode *root)
+    {
+
+        if (!root)
+            return NULL;
+
+        increasingBST(root->left);
+
+        if (newroot == NULL)
+        {
+            newroot = new TreeNode(root->val);
+            curr = newroot;
+        }
+        else
+        {
+            curr->right = new TreeNode(root->val);
+            curr = curr->right;
+        }
+
+        increasingBST(root->right);
+
+        return newroot;
+    }
+};
