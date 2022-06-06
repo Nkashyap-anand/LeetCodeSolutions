2339d46165133b91e07ffb216be667e311b56ec8
Nkashyap-anand <nkashyap.anand@gmail.com>
2022-06-06 09:08:00 +0530

    52. N-Queens II

---
+++ N-Queens ii.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    bool isSafe(int row, int col, vector<string> board, int n)
+    {
+        int duprow = row;
+        int dupcol = col;
+        
+        while(row>=0 && col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            row--;
+            col--;
+        }
+        
+        row = duprow;
+        col = dupcol;
+        
+        while(col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+        
+        row = duprow;
+        col = dupcol;
+        
+        while(row<n && col>=0)
+        {
+            if(board[row][col]=='Q') return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+    
+    int solve(int col, vector<string> &board, vector<vector<string>> &ans, int n)        
+    {
+        int count = 0;
+        if(col==n){
+            ans.push_back(board);
+            return 1;
+        }
+        
+        for(int row=0; row<n; row++)
+        {
+            if(isSafe(row, col, board, n))
+            {
+                board[row][col] = 'Q';              
+                count += solve(col+1, board, ans,n);
+                board[row][col] = '.';
+            }
+        }
+    
+        return count;
+    }
+    
+    
+     int totalNQueens(int n) {
+        
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+ 
+        for(int i=0; i<n; i++)
+        {
+            board[i]=s;
+        }
+        
+       int sol = solve(0, board, ans, n);
+           return sol;  
+    }
+};
+
+
+//// 2nd approach
