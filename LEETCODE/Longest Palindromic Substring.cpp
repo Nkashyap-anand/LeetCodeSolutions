323f954967b5d2227f1b30d02ba38a6a6d9ac7fe
nkashyap <nkashyap.anand@gmail.com>
2022-06-16 18:59:27 +0530

    5. Longest Palindromic Substring

---
+++ LEETCODE/Longest Palindromic Substring.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        if(s.size() <= 1) return s;
+        int n = s.size();
+        int start=0, end = 0, max_len = 1;
+        
+        // odd length
+        
+        for(int i=0; i<n-1; i++)
+        {
+           int l = i, r=i;
+            
+            while(l>=0 && r<n)
+            {
+                if(s[l]==s[r])
+                {
+                    l--;
+                    r++;
+                }
+                else break;
+            }
+            
+            int len = r-l-1;
+            
+            if(len>max_len)
+            {
+                max_len = len;
+                start = l+1;
+                end = r-1;
+            }
+        }
+        
+        //even length
+        for(int i=0; i<n-1; i++)
+        {
+           int l = i, r=i+1;
+            
+            while(l>=0 && r<n)
+            {
+                if(s[l]==s[r])
+                {
+                    l--;
+                    r++;
+                }
+                else break;
+            }
+            
+            int len = r-l-1;
+            
+            if(len>max_len)
+            {
+                max_len = len;
+                start = l+1;
+                end = r-1;
+            }
+        }
+        
+        return s.substr(start, max_len);
+    }
+};
