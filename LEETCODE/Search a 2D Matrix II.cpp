44353e451339759da644073419689080dddd69bb
nkashyap <nkashyap.anand@gmail.com>
2022-07-30 12:57:39 +0530

    240. Search a 2D Matrix II v2

--- LEETCODE/Search a 2D Matrix II.cpp
+++ LEETCODE/Search a 2D Matrix II.cpp
@@ -35,3 +35,33 @@ public:
         return false;
     }
 };
+
+// simlified sol
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+
+        if (matrix.size() == 0)
+            return 0;
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int rowId = 0, colId = col - 1;
+
+        while (rowId < row && colId >= 0)
+        {
+            int elm = matrix[rowId][colId];
+
+            if (elm == target)
+                return 1;
+            if (elm < target)
+                rowId++;
+            else
+                colId--;
+        }
+        return 0;
+    }
+};
