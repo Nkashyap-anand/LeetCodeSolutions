424446c33df33fbb24d6b08f1c94686ddce21638
nkashyap <nkashyap.anand@gmail.com>
2022-08-15 12:44:41 +0530

    350. Intersection of Two Arrays II

---
+++ LEETCODE/Intersection of Two Arrays II.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        map<int, int> freq;
+        vector<int> v;
+
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            freq[nums1[i]]++;
+        }
+
+        for (int i = 0; i < nums2.size(); i++)
+        {
+            if (freq[nums2[i]] > 0)
+            {
+                freq[nums2[i]]--;
+                v.push_back(nums2[i]);
+            }
+        }
+
+        return v;
+    }
+};
