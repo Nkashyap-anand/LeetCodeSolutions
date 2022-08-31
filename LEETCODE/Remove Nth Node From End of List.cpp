59eb40e12486c8b73963ce6d766522758137b1de
nkashyap <nkashyap.anand@gmail.com>
2022-08-31 19:58:51 +0530

    19. Remove Nth Node From End of List
    tortoise approach

--- LEETCODE/Remove Nth Node From End of List.cpp
+++ LEETCODE/Remove Nth Node From End of List.cpp
@@ -41,3 +41,30 @@ public:
         return head;
     }
 };
+
+// tortoise approach
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+
+        ListNode *start = new ListNode();
+        start->next = head;
+        ListNode *slow = start;
+        ListNode *fast = start;
+
+        for (int i = 0; i < n; i++)
+            fast = fast->next;
+
+        while (fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return start->next;
+    }
+};
