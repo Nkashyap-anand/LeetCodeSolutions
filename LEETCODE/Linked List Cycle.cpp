a2b6ccf2cf970b9a0753b4a2e1491a6db02be324
nkashyap <nkashyap.anand@gmail.com>
2022-09-02 11:09:19 +0530

    141. Linked List Cycle

---
+++ LEETCODE/Linked List Cycle.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+
+        if (head == NULL || head->next == NULL)
+            return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+};
