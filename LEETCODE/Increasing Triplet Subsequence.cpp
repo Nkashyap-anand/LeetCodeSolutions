fd2275e6f6a550e0adbcb48ed3426d4e4123b778
nkashyap <nkashyap.anand@gmail.com>
2022-10-11 18:36:50 +0530

    334. Increasing Triplet Subsequence

---
+++ LEETCODE/Increasing Triplet Subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool increasingTriplet(vector<int> &nums)
+    {
+
+        if (nums.size() < 3)
+            return false;
+
+        int left = INT_MAX;
+        int mid = INT_MAX;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > mid)
+                return true;
+
+            if (nums[i] > left && nums[i] < mid)
+                mid = nums[i];
+
+            if (nums[i] < left)
+                left = nums[i];
+        }
+
+        return false;
+    }
+};
