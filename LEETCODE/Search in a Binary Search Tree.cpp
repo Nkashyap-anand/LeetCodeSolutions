1ef0e2dbe192ebd7098c12f844664544a4140e73
nkashyap <nkashyap.anand@gmail.com>
2023-02-02 20:52:57 +0530

    700. Search in a Binary Search Tree

--- LEETCODE/Search in a Binary Search Tree.cpp
+++ LEETCODE/Search in a Binary Search Tree.cpp
@@ -16,3 +16,17 @@ public:
             return searchBST(root->right, val);
     }
 };
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+
+        while (root != NULL && root->val != val)
+        {
+            root = (val < root->val) ? root->left : root->right;
+        }
+        return root;
+    }
+};
