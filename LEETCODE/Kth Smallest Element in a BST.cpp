1cae3d05570658f0f77b05f7fc91875468c55518
nkashyap <nkashyap.anand@gmail.com>
2023-02-05 17:49:44 +0530

    230. Kth Smallest Element in a BST

--- LEETCODE/Kth Smallest Element in a BST.cpp
+++ LEETCODE/Kth Smallest Element in a BST.cpp
@@ -26,3 +26,29 @@ public:
         return v[k - 1];
     }
 };
+
+// without extra space
+class Solution
+{
+public:
+    int ans = 0;
+    int kthSmallest(TreeNode *root, int k)
+    {
+        inorder(root, k);
+        return ans;
+    }
+
+    void inorder(TreeNode *root, int &k)
+    {
+        if (!root)
+            return;
+
+        inorder(root->left, k);
+        if (--k == 0)
+        {
+            ans = root->val;
+            return;
+        }
+        inorder(root->right, k);
+    }
+};
